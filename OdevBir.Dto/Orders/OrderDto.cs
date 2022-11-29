d9d58a3bf351f274b281dafb70ae4702659fbeb6
MRaifAydin <mert5708@gmail.com>
2022-11-29 21:30:24 +0300

    Added dto classes.

---
+++ OdevBir.Dto/Orders/OrderDto.cs
@@ -0,0 +1,21 @@
+﻿using OdevBir.Dto.Customers;
+using OdevBir.Dto.Employees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdevBir.Dto.Orders
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+
+        public CustomerDto Customer { get; set; }
+
+        public EmployeeDto Employee { get; set; }
+
+        public DateTime? OrderDate { get; set; }
+    }
+}
