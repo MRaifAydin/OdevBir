14c59220065b5c4b4f32dd77d366c4db769c5d46
MRaifAydin <mert5708@gmail.com>
2022-11-29 17:08:44 +0300

    MVC added.

---
+++ OdevBir.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OdevBir.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
